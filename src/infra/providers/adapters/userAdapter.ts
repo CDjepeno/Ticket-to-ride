2316481e089a77504afab79c83b87ea97dd80a1c
CDjepeno <djepeno.christopher@gmail.com>
2021-11-04 17:45:23 +0100

    create getUsers adapter for infrastructure

--- src/infra/providers/adapters/userAdapter.ts
+++ src/infra/providers/adapters/userAdapter.ts
@@ -1,4 +1,5 @@
 import { AddUser } from '../../../core/application/useCase/user/addUser/AddUser';
+import { GetUsers } from '../../../core/application/useCase/user/getUsers/getUsers';
 import { UserProvider } from '../UserProvider';
 
 
@@ -6,5 +7,6 @@ import { UserProvider } from '../UserProvider';
 const userRepository = new UserProvider()
 
 const addUserInteractor = new AddUser(userRepository)
+const getUsersInteractor = new GetUsers(userRepository)
 
 export {addUserInteractor}
