09cbeaf7eda2f7edabf94232dedf7a7d0eae6403
CDjepeno <djepeno.christopher@gmail.com>
2021-11-05 14:27:21 +0100

    Infra: create middleware for ownerComment

--- src/infra/middlewares/ownerComment.ts
+++ src/infra/middlewares/ownerComment.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { getRepository } from 'typeorm';
-import { Ticket } from '../models/Ticket';
+import { Comment } from '../models/Comment';
 import { User } from '../models/User';
 import { validateEmail } from '../utils/validateEmail';
 
 
-export const ownerTicket = async (
+export const ownerComment = async (
   request: Request,
   response: Response,
   next: NextFunction
@@ -14,7 +14,7 @@ export const ownerTicket = async (
 
   if (!authorizationHeader) {
     const message = "You don't give an email in header";
-    return response.status(403).json({ message });
+    return response.status(401).json({ message });
   }
 
   const email = authorizationHeader.split(" ")[1];
@@ -23,20 +23,20 @@ export const ownerTicket = async (
     const message = "your email is invalid";
     return response.status(403).json({ message });
   }
-  
+
   const idTicket = request.params.id;
 
-  const ticket = await getRepository(Ticket).findOne(idTicket);
+  const comment = await getRepository(Comment).findOne(idTicket);
   const user = await getRepository(User).findOne({ where: { email: email } });
 
-  if (!ticket || !user) {
-    const message = "ticket or user not found";
-    return response.status(404).json({ message });
+  if(!comment || !user) {
+    const message = "comment or user not found";
+    return response.status(404).json({ message })
   }
 
-  if (ticket.userId !== user.id) {
-    const message = "You are not owner for this ticket";
-    return response.status(403).json({ message }); 
+  if (comment.userId !== user.id) {
+    const message = "You are not owner for this comment";
+    return response.status(403).json({ message });
   } else {
     next();
   }
--- src/infra/routes/commentRoutes.ts
+++ src/infra/routes/commentRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { CommentController } from '../controller/CommentController'
+import { ownerComment } from '../middlewares/ownerComment'
 
 const commentRoutes = Router()
 
 commentRoutes.get('/ticket/:id/comments', CommentController.getCommentsForOneTicket)
-commentRoutes.delete('/comments/:id', CommentController.delete)
+commentRoutes.delete('/comments/:id',ownerComment, CommentController.delete)
 commentRoutes.post('/comments', CommentController.save)
-commentRoutes.put('/comments/:id', CommentController.update)
+commentRoutes.put('/comments/:id',ownerComment, CommentController.update)
 
 export default commentRoutes
